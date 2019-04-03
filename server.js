d2375cf53b5a80b94fda5fe84d7a9c9c80a01190
Olivier Martineau <omartineau8@gmail.com>
2019-04-03 14:21:22 -0400

    added environment variables

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const passport   = require('passport');
+const session    = require('express-session');
+const bodyParser = require('body-parser');
+const env = require('dotenv').load();
+
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 const app = express();
+
 
 
 app.get('/', function(req, res) {
@@ -16,3 +28,11 @@ app.listen(5000, function(err) {
     else console.log(err)
 
 });
+
+// For Passport
+
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessions
