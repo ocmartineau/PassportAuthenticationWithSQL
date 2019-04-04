48159d831a6a7d913aac129b6de0152d21fa23da
Olivier Martineau <omartineau8@gmail.com>
2019-04-04 19:28:45 -0400

    added signup and signin form views

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -5,3 +5,7 @@ exports.signup = function(req, res) {
     res.render('signup');
 
 };
+
+exports.signin = function(req, res) {
+    res.render('signin');
+}
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,4 +4,7 @@ module.exports = function(app) {
 
     app.get('/signup', authController.signup);
 
+    app.get('/signin', authController.signin);
+
 }
+
